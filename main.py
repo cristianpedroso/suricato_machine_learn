1260d29caa3980adaf489adfc0947572757f272c
Thiago Oliveira <thiagoariolimendes@gmail.com>
2017-04-29 21:07:47 -0300

    add inundation

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ from models import *
 if __name__ == "__main__":
     print("Hello")
     print(Preciptation.query())
+    print(Inundation.query())
--- models.py
+++ models.py
@@ -13,4 +13,18 @@ class Preciptation:
 
         with db.connect() as cur:
             cur.execute(sql)
-            return [Preciptation(row) for row in cur]
+            return [cls(row) for row in cur]
+
+class Inundation:
+    def __init__(self, db_row):
+        (self.date, self.city) = db_row
+        # (date, city, value) = db_row
+        # self.date = date
+
+    @classmethod
+    def query(cls):
+        sql = 'SELECT inundation.date_hour , city.name FROM suricato.inundation INNER JOIN suricato.city ON city.pk_city = inundation.fk_city'
+
+        with db.connect() as cur:
+            cur.execute(sql)
+            return [cls(row) for row in cur]
