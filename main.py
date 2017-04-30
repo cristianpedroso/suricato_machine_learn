ee5781bfcecad9ffe4d567800659b2832c9ffdd5
Thiago Oliveira <thiagoariolimendes@gmail.com>
2017-04-29 21:16:21 -0300

    add LunarPhase

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ if __name__ == "__main__":
     print("Hello")
     print(Preciptation.query())
     print(Inundation.query())
+    print(LunarPhase.position())
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import db
+import math, decimal, datetime
 
 
 class Preciptation:
@@ -15,6 +16,7 @@ class Preciptation:
             cur.execute(sql)
             return [cls(row) for row in cur]
 
+
 class Inundation:
     def __init__(self, db_row):
         (self.date, self.city) = db_row
@@ -28,3 +30,34 @@ class Inundation:
         with db.connect() as cur:
             cur.execute(sql)
             return [cls(row) for row in cur]
+
+
+class LunarPhase:
+    @classmethod
+    def position(cls, now=None):
+        if now is None:
+            now = datetime.datetime.now()
+        dec = decimal.Decimal
+        diff = now - datetime.datetime(2001, 1, 1)
+        days = dec(diff.days) + (dec(diff.seconds) / dec(86400))
+        lunations = dec("0.20439731") + (days * dec("0.03386319269"))
+        lunations = lunations % dec(1)
+
+        phasename = cls.phase(lunations)
+        roundedpos = round(float(lunations), 3)
+        return (phasename, roundedpos)
+
+    def phase(pos):
+        dec = decimal.Decimal
+        index = (pos * dec(8)) + dec("0.5")
+        index = math.floor(index)
+        return {
+            0: "New Moon",
+            1: "Waxing Crescent",
+            2: "First Quarter",
+            3: "Waxing Gibbous",
+            4: "Full Moon",
+            5: "Waning Gibbous",
+            6: "Last Quarter",
+            7: "Waning Crescent"
+        }[int(index) & 7]
